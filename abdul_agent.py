253d2f02c609ad1c6ed6d45fd7bf170ad77bdbeb
Abdul Shaltu <abdul.shaltu17@gmail.com>
2019-01-25 23:27:38 -0800

    Add Abdul's Agent Feel free to mess around and make changes, I know there are some redundant bits in my code.

---
+++ abdul_agent.py
@@ -0,0 +1,56 @@
+import gym
+import numpy as np
+from gym import wrappers
+
+environment = gym.make('CartPole-v0')
+environment._max_episode_steps = 10000
+
+best_steps = 0
+episode_steps = []
+
+best_weights = np.zeros(4)
+
+def saveVideoOfBest(environment):
+    finished = False
+    count = 0
+    environment = wrappers.Monitor(environment,'BestOne',force = True)
+    state = environment.reset()
+    while not finished:
+        count += 1
+        action = 1 if np.dot(state, best_weights) > 0 else 0
+        state, reward, finished, debug = environment.step(action)
+
+        if finished:
+            break
+
+for i in range(1000):
+    new_weights = np.random.uniform(-1.0, 1.0, 4)
+
+    steps = []
+    for j in range(1000):
+        state = environment.reset()
+        done = False
+        step_counter = 0
+    while not done:
+        # environment.render()
+        step_counter += 1
+        action = 1 if np.dot(state, new_weights) > 0 else 0
+        state, reward, done, _ = environment.step(action)
+
+        if done:
+            break
+
+    steps.append(step_counter)
+    average_steps = float(sum(steps) / len(steps))
+    if average_steps > best_steps:
+        best_steps = average_steps
+        best_weights = new_weights
+        if best_steps == 10000:
+            episode_steps.append(average_steps)
+            print('Episode',i+1,'Average # of Steps:',average_steps,'\t\tBest Weight:',best_weights)
+            break
+    episode_steps.append(average_steps)
+    print('Episode',i+1,'Average # of Steps:',average_steps,'\t\tBest Weight:',best_weights)
+
+saveVideoOfBest(environment)
+environment.close()
